3de91998bea5e44bf75fe6012b5e2ed321afb946
Adrian-Koh <adriankoh216@yahoo.com>
2025-07-15 13:34:52 +0800

    Implement randomize sequence upon card clicked
    Also add score which resets after same card is clicked twice

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,23 +1,47 @@
 import { useEffect, useState } from "react";
-import cardsInfo from "./cards-info";
+import { cardsInfo, IMAGE_COUNT, randomizeSequence } from "./cards-info";
 import "../styles/cards.css";
 
 export default function Cards() {
   const [infos, setInfos] = useState([]);
+  const [clickedIds, setClickedIds] = useState(
+    new Array(IMAGE_COUNT).fill(false),
+  );
 
   useEffect(() => {
-    let update = async () => {
+    let fetchImages = async () => {
       let result = await cardsInfo();
       setInfos(result);
     };
-    update();
+    fetchImages();
   }, []);
 
+  function handleClick(id) {
+    let newClickedIds;
+    if (clickedIds[id]) {
+      // reset score
+      newClickedIds = new Array(IMAGE_COUNT).fill(false);
+    } else {
+      newClickedIds = [...clickedIds];
+      newClickedIds[id] = true;
+    }
+    setClickedIds(newClickedIds);
+    setInfos(randomizeSequence(infos));
+  }
+
   return (
-    <div className="grid">
-      {infos.map((info) => (
-        <img src={info.imageUrl} key={info.name}></img>
-      ))}
+    <div className="container">
+      <div className="score">
+        Score: {clickedIds.filter((clicked) => clicked).length}
+      </div>
+      <div className="grid">
+        {infos.map((info) => (
+          <div className="card" key={info.id}>
+            <img src={info.imageUrl} onClick={() => handleClick(info.id)}></img>
+            <p>{info.name}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/cards-info.js
+++ src/components/cards-info.js
@@ -1,14 +1,34 @@
-export default async function cardsInfo() {
-  const response = await fetch("https://api.jikan.moe/v4/anime/666/characters");
+const API_LINK = "https://api.jikan.moe/v4/anime/666/characters";
+const IMAGE_COUNT = 12;
+
+async function cardsInfo() {
+  const response = await fetch(API_LINK);
   const parsed = await response.json();
   const data = parsed.data;
 
   let result = [];
-  for (let i = 0; i < Math.min(12, data.length); i++) {
+  for (let i = 0; i < Math.min(IMAGE_COUNT, data.length); i++) {
     result.push({
+      id: i,
       imageUrl: data[i].character.images.jpg.image_url,
       name: data[i].character.name,
     });
   }
   return result;
 }
+
+function randomizeSequence(arr) {
+  let newArr = new Array(arr.length).fill(null);
+
+  for (let i = 0; i < arr.length; i++) {
+    let randomNumber;
+    do {
+      randomNumber = Math.floor(Math.random() * 12);
+    } while (newArr[randomNumber] !== null);
+    newArr[randomNumber] = arr[i];
+  }
+
+  return newArr;
+}
+
+export { cardsInfo, randomizeSequence, IMAGE_COUNT };
